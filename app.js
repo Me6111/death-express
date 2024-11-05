2cf2bb58850286fbb7c5388f193b581e5db00091
Me6111 <maksymmpawlowski@gmail.com>
2024-11-05 19:04:42 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
-// C:\Users\user\Desktop\projects\Death\death-express\app.js
-
 const express = require('express');
 const cors = require('cors');
-const TablesInfoService = require('./tablesInfo'); // Ensure this path is correct
+const TablesInfoService = require('./tablesInfo');
 const app = express();
 const db = require('./db');
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// Allow CORS from all origins
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 const tablesInfoService = new TablesInfoService(process.env.DATABASE_URL);
-
-
-
-
 
 // Root path route
 app.get('/', (req, res) => {
   res.send('Hello from the root path!');
 });
 
-
-
-
-
-
 // Hello route
 app.get('/hello', (req, res) => {
   res.send('Hello client');
 });
 
-
-
-
-
-
 // Tables-info route
 app.get('/tables-info', async (req, res) => {
   try {
     const tablesInfo = await tablesInfoService.fetchTablesAndColumns();
-    // Directly send the structured data obtained from fetchTablesAndColumns
     res.json(tablesInfo);
   } catch (error) {
     console.error('Error fetching tables and columns:', error);
@@ -49,7 +34,6 @@ app.get('/tables-info', async (req, res) => {
   }
 });
 
-
-
-
-
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
